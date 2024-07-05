342acbabc2e5b12e45745fd61fb7f9057e31540f
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-07-05 21:49:16 +0530

    init

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app = express();
 
 app.use(cors({
-    origin: 'frontend-theta-steel-11.vercel.app',
+    origin: 'https://frontend-theta-steel-11.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
