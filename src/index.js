80278716bf29758c371ef732a181219258454094
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-07-05 21:27:58 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import { app } from "./app.js";
 
 app.listen(8000, () => {
-    console.log("Listening on port 8000");
-})
+  console.log("Listening on port 8000");
+});
 
-app.post('/api/cookie', (req, res) => {
-    console.log('Setting cookies');
-    const options = {
-        httpOnly: true,
-        domain: 'https://frontend-theta-steel-11.vercel.app',
-        path: '/',
-        sameSite: 'None',
-        secure: true,
-    };
-    return res.status(200).cookie('TestCookie', 'helloIamatestcookie', options).json({ message: 'Cookies set'})
-})
+app.post("/api/cookie", (req, res) => {
+  console.log("Setting cookies");
+  const options = {
+    httpOnly: true,
+    domain: "https://frontend-theta-steel-11.vercel.app",
+    path: "/",
+    sameSite: "None",
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("TestCookie", "helloIamatestcookie", {
+      httpOnly: true,
+      domain: "https://frontend-theta-steel-11.vercel.app",
+      path: "/",
+      sameSite: "none",
+      secure: true,
+    })
+    .json({ message: "Cookies set" });
+});
